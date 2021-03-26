c78e24e9d772ef37bda1dfd07cc885aa0beff2c3
Roman Khodukin <romakhodukin@gmail.com>
2021-03-26 03:56:51 +0300

    Add Firebase and Swiftlint

---
+++ ChatApp/Managers/OperationAsyncManager.swift
@@ -0,0 +1,93 @@
+//
+//  OperationDataManager.swift
+//  ChatApp
+//
+//  Created by Roman Khodukin on 3/18/21.
+//
+
+import UIKit
+
+class OperationAsyncManager: DataManager {
+    
+    // MARK: - Private properties
+    
+    private lazy var operationQueue: OperationQueue = {
+        let queue = OperationQueue()
+        queue.qualityOfService = .utility
+        return queue
+    }()
+    
+    // MARK: - DataManager methods
+    
+    func loadUserData(completion: @escaping (UserViewModel?) -> Void) {
+        let operation = LoadUserDataOperation(completion: completion)
+        operationQueue.addOperation(operation)
+    }
+    
+    func saveUserData(_ user: UserViewModel, completion: ((Bool) -> Void)? = nil) {
+        let operation = SaveUserDataOperation(userViewModel: user, completion: completion)
+        operationQueue.addOperation(operation)
+    }
+    
+    // MARK: - Operation classes
+    
+    class LoadUserDataOperation: Operation {
+        var completion: (UserViewModel?) -> Void
+        
+        init(completion: @escaping (UserViewModel?) -> Void) {
+            self.completion = completion
+        }
+        
+        override func main() {
+            guard !isCancelled,
+                  let data = FileManagement.readFromDisk(fileName: Constants.userDataFileName),
+                  let user = try? JSONDecoder().decode(User.self, from: data)
+            else { return completion(nil) }
+            
+            guard !isCancelled else { return completion(nil) }
+            
+            var image: UIImage?
+            if let profileImageUrl = user.profileImageUrl,
+               let imageData = FileManagement.read(url: profileImageUrl) {
+                image = UIImage(data: imageData)
+            }
+            
+            completion(.init(fullName: user.fullName, description: user.description, profileImage: image))
+        }
+    }
+    
+    class SaveUserDataOperation: Operation {
+        
+        private var userViewModel: UserViewModel
+        private var completion: ((Bool) -> Void)?
+        
+        init(userViewModel: UserViewModel, completion: ((Bool) -> Void)? = nil) {
+            self.userViewModel = userViewModel
+            self.completion = completion
+        }
+        
+        override func main() {
+            var imageSavedSuccessfully = false
+            var dataSavedSuccessfully = false
+            
+            var imageUrl: URL?
+            if let imageData = userViewModel.profileImage?.pngData() {
+                (imageSavedSuccessfully, imageUrl) =
+                    FileManagement.writeToDisk(data: imageData, fileName: Constants.userImageFileName)
+            } else if userViewModel.profileImage == nil {
+                imageSavedSuccessfully = true
+            }
+            
+            let user = User(fullName: userViewModel.fullName, description: userViewModel.description, profileImageUrl: imageUrl)
+            
+            if let data = try? JSONEncoder().encode(user),
+               FileManagement.writeToDisk(data: data, fileName: Constants.userDataFileName).isSuccessful {
+                dataSavedSuccessfully = true
+            }
+            
+            if let completion = completion {
+                completion(imageSavedSuccessfully && dataSavedSuccessfully)
+            }
+        }
+    }
+}
---
+++ Podfile
@@ -0,0 +1,24 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '12.0'
+
+target 'ChatApp' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  # use_frameworks!
+
+  # Pods for ChatApp
+
+  pod 'Firebase/Firestore', '~> 7.8'
+  pod 'SwiftLint'
+
+  # ignore all warnings from all dependencies
+  inhibit_all_warnings!
+
+end
+
+post_install do |installer|
+  installer.pods_project.targets.each do |target|
+    target.build_configurations.each do |config|
+      config.build_settings.delete 'IPHONEOS_DEPLOYMENT_TARGET'
+    end
+  end
+end
